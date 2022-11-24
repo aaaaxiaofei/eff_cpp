9a14e11e9fdb1126e590a2c1348fbd80ef0976f9
fei <aaaaxiaofei@163.com>
2022-11-24 16:51:12 +0800

    Item1

---
+++ src/item1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    cout << "C++ is a combination of \n" 
+         << "- C \n"
+         << "- Object Oriented C++ \n"
+         << "- Template C++ \n"
+         << "- STL"
+         << endl;
+    return 0;
+}
